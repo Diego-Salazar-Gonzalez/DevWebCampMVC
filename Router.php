b9feb326dfee1da493eacfa36af76a2c746cb70d
Diego Salazar <diegosalazargonzalez2017@gmail.com>
2022-11-07 09:29:30 -0500

    ultimos ajustes

--- Router.php
+++ Router.php
@@ -66,7 +66,7 @@ class Router
 
         // Utilizar el Layout de acuerdo a la URL
         $url_actual = $_SERVER['PATH_INFO'] ?? '/';
-
+        echo $url_actual;
         if(str_contains($url_actual, '/admin')) {
             include_once __DIR__ . '/views/admin-layout.php';
         } else {
